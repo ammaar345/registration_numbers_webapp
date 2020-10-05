01e2b33102d6abd26490e2df458be8bd966005e6
Ammaar <jodifortuin@gmail.com>
2020-10-05 15:05:59 +0200

    changed column name

--- tbl.sql
+++ tbl.sql
@@ -1,12 +1,9 @@
 create table towns (id serial not null primary key , townName text,town text);
-
-
-
 create table regNumbers (
 id serial not null primary key,
-regNum int,
+regNumId int,
 reg text,
-foreign key (regNum) references towns(id)
+foreign key (regNumId) references towns(id)
 );
 
 -- //remove townName or town if not needed
